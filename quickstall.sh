b6cdfc7fb4f83e26354464b7c7327cc65b972070
strotic <86202527+strotic@users.noreply.github.com>
2021-06-25 19:39:52 -0400

    Beta

---
+++ quickstall.sh
@@ -0,0 +1,8 @@
+echo "Fixing something....."
+
+chmod +x gs.sh  
+chmod +x ud.sh
+chmod +x puntu 
+echo "Getting libraries..." pip3 install string-color > /dev/null; echo "Installation finished :)"
+sleep 2
+echo "Done!"
---
+++ ud.sh
@@ -0,0 +1,1 @@
+echo "Feature coming soon. :) "
